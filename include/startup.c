0316026bd71d30132aebcfb9a2c7c92b541253ad
glankk <tommy.bergman91@gmail.com>
2017-11-28 11:43:27 +0100

    bugfix in `clear_bss`

--- include/startup.c
+++ include/startup.c
@@ -16,19 +16,23 @@ static __attribute__((section(".dtor_i"), used)) int32_t dtor_i = -1;
 static __attribute__((section(".dtor_n"), used)) int32_t dtor_n = 0;
 #endif
 
+extern __attribute__((section(".bss"))) void *__bss_end;
+extern __attribute__((section(".bss"))) void *__bss_start;
+
+extern void (*__CTOR_LIST__[])(void);
+extern void (*__DTOR_LIST__[])(void);
+
 void clear_bss(void)
 {
-  extern void *__bss_start, *__bss_end;
-  uint8_t *bss_start = (uint8_t*)&__bss_start;
-  uint8_t *bss_end = (uint8_t*)&__bss_end;
-  while (bss_start < bss_end)
-    *bss_start++ = 0x00;
+  char *p = (char*)&__bss_start;
+  char *e = (char*)&__bss_end;
+  while (p < e)
+    *p++ = 0;
 }
 
 void do_global_ctors(void)
 {
-  extern void (*__CTOR_LIST__[])(void);
-  static int global_ctors_done;
+  static _Bool global_ctors_done;
   if (global_ctors_done)
     return;
   int32_t i = (int32_t)__CTOR_LIST__[0];
@@ -42,8 +46,7 @@ void do_global_ctors(void)
 
 void do_global_dtors(void)
 {
-  extern void (*__DTOR_LIST__[])(void);
-  static int global_dtors_done;
+  static _Bool global_dtors_done;
   if (global_dtors_done)
     return;
   for (void (**dtor)(void) = &__DTOR_LIST__[1]; *dtor; ++dtor)
