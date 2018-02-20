4457f299c80cc95400f049b9cba35747a05df856
glankk <tommy.bergman91@gmail.com>
2018-02-20 20:43:57 +0100

    fixed libgru compilation errors

--- src/libgru/src/lib/os.c
+++ src/libgru/src/lib/os.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
 #include <errno.h>
+#include <unistd.h>
 #include <dirent.h>
 #include <sys/stat.h>
 #include "gru.h"
