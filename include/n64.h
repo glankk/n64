cf6c596746c3c927312f3fc5f1be72587a75aff4
glank <tommy.bergman91@gmail.com>
2018-08-18 15:18:46 +0200

    add n64/vr4300.h

--- include/n64.h
+++ include/n64.h
@@ -5,10 +5,11 @@
 #ifndef N64_H
 #define N64_H
 
-#include "n64/task.h"
-#include "n64/thread.h"
+#include "n64/gbi.h"
 #include "n64/message.h"
 #include "n64/pi.h"
-#include "n64/gbi.h"
+#include "n64/task.h"
+#include "n64/thread.h"
+#include "n64/vr4300.h"
 
 #endif
